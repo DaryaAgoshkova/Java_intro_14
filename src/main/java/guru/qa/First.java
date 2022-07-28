53ed662515e39f2c8185e2473c66b18e07f33311
Дарья Агошкова <dariaagoskova@MacBook-Air-Dara.local>
2022-07-28 22:09:03 +0300

    init commit

---
+++ src/main/java/guru/qa/First.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+public class First {
+
+    public static void main(String[] args) {
+        String str = "Dasha";
+        Person student = new Person();
+        student.age = 34;
+        student.name = "Dasha";
+        student.goToPension();
+        student.sayName();
+    }
+}
+
+
